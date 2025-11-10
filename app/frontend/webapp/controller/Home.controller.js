ba624fde580ad8585b93e7fe83a40157d2b490bf
Lokesh Ghosh <lokesh.ghosh@wipro.com>
2025-11-10 18:20:29 +0000

    add navbar

--- app/frontend/webapp/controller/Home.controller.js
+++ app/frontend/webapp/controller/Home.controller.js
@@ -1,10 +1,64 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel",
+], (Controller, Fragment, JSONModel) => {
     "use strict";
 
     return Controller.extend("com.taskflow.dev.frontend.controller.Home", {
         onInit() {
+            this.oFragmentModel = new JSONModel({
+                key: "home"
+            })
+            this.getView().setModel(this.oFragmentModel, "fragmentModel");
+            this._openHomeFragment({ key: "home" });
+        },
+        onNavSelect(oEvent) {
+            var sNavItemKey = oEvent.getParameters("items")['item'].getKey()
+
+            this.getView().byId("heroLoadArea").setBusy(true);
+            this._openHomeFragment({ key: sNavItemKey });
+
+        },
+        _openHomeFragment(oData) {
+            var oView = this.getView();
+            this.oFragmentModel.setData(oData);
+            var sNavItemKey = oData.key;
+            // console.log("htye",sNavItemKey);
+
+            if (this._userFormDialog) {
+                console.log("item already exists");
+              
+                oView.byId("heroLoadArea").destroyItems();
+
+            }
+
+            if (!this._userFormDialog) {
+               this._loadFragment(sNavItemKey);
+            }else{
+                oView.byId("heroLoadArea").destroyItems();
+
+               this._loadFragment(sNavItemKey);
+            }
+
+            this._userFormDialog.then((oFragment) => {
+
+                oView.byId("heroLoadArea").addItem(oFragment);
+                this.getView().byId("heroLoadArea").setBusy(false);
+            })
+        },
+
+        _loadFragment(sNavItemKey){
+            var oView = this.getView();
+             this._userFormDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: `com.taskflow.dev.frontend.view.fragments.${sNavItemKey}`,
+                    controller: this
+                }).then(function (oFragment) {
+                    oView.addDependent(oFragment);
+                    return oFragment;
+                })
         }
+
     });
 });
